b0d3c4504a9b582ee617937d035285ee92845fec
pen2000 <ohno.pengvc@icloud.com>
2022-06-14 09:47:43 +0900

    フェードアニメーションの変更。

--- js/fade.js
+++ js/fade.js
@@ -15,10 +15,12 @@ function fadeAnime() {
 
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
-    fadeAnime();/* アニメーション用の関数を呼ぶ*/
-});// ここまで画面をスクロールをしたら動かしたい場合の記述
+    // アニメーション用の関数を呼ぶ
+    fadeAnime();
+});
 
 // 画面が読み込まれたらすぐに動かしたい場合の記述
 $(window).on('load', function () {
-    fadeAnime();/* アニメーション用の関数を呼ぶ*/
-});// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+    // アニメーション用の関数を呼ぶ
+    window.setTimeout(fadeAnime, 500);
+});
